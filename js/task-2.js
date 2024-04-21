0278414a5565512d126df2cef23cf0bf67a66a91
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-04-21 16:21:27 +0300

    fix1

--- js/task-2.js
+++ js/task-2.js
@@ -24,54 +24,70 @@ const images = [
     alt: "Lighthouse Coast Sea",
   }
 ];
+
+let counter = 0;
+
 const galleryArray = document.querySelector("ul.gallery");
-const firstItem = document.createElement("li");
-firstItem.classList.add("gallery-list-1");
-const secondItem = document.createElement("li");
-secondItem.classList.add("gallery-list-2");
-const thirdItem = document.createElement("li");
-thirdItem.classList.add("gallery-list-3");
-document.body.append(firstItem, secondItem, thirdItem);
+images.forEach(function callback(element, index, array) {
+  if (counter >= 3) {
+    return;
+  }
+  const newEl = document.createElement("li");
+  newEl.classList.add("gallery-list");
+  galleryArray.append(newEl);
+  
+  const imageEl = document.createElement("img");
+  imageEl.src = element.url;
+  imageEl.alt = element.alt;
+  imageEl.width = 360;
+  imageEl.height = 300;
 
-const imageE1 = document.createElement("img");
-imageE1.src = "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260";
-imageE1.alt = "White and Black Long Fur Cat";
-imageE1.width = 360;
-imageE1.height = 300;
-
-
-
-const galleryList1 = document.querySelector(".gallery-list-1");
-galleryList1.append(imageE1);
-
-const imageE2 = document.createElement("img");
-imageE2.src = "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260";
-imageE2.alt = "Orange and White Koi Fish Near Yellow Koi Fish";
-imageE2.width = 360;
-imageE2.height = 300;
+  newEl.append(imageEl);
+    
+  counter++;
+})
 
 
 
-const galleryList2 = document.querySelector(".gallery-list-2");
-galleryList2.append(imageE2);
-
-
-const imageE3 = document.createElement("img");
-imageE3.src = "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260";
-imageE3.alt = "Group of Horses Running";
-imageE3.width = 360;
-imageE3.height = 300;
-
-const galleryList3 = document.querySelector(".gallery-list-3");
-galleryList3.append(imageE3);
 
 
 
-//secondItem.src = `https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260`;
 
-//console.log(firstItem)
-// firstItem.append("img1, img2, img3")
-//img1.src = `https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260`;
-// img2.src = "<https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260>";
-// img3.src = "<ttps://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260>";
-//image.src = "<>";
+//const galleryArray = document.querySelector("ul.gallery");
+// const firstItem = document.createElement("li");
+// firstItem.classList.add("gallery-list-1");
+// const secondItem = document.createElement("li");
+// secondItem.classList.add("gallery-list-2");
+// const thirdItem = document.createElement("li");
+// thirdItem.classList.add("gallery-list-3");
+// document.body.append(firstItem, secondItem, thirdItem);
+
+// const imageE1 = document.createElement("img");
+// imageE1.src = "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260";
+// imageE1.alt = "White and Black Long Fur Cat";
+// imageE1.width = 360;
+// imageE1.height = 300;
+
+
+// const galleryList1 = document.querySelector(".gallery-list-1");
+// galleryList1.append(imageE1);
+
+// const imageE2 = document.createElement("img");
+// imageE2.src = "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260";
+// imageE2.alt = "Orange and White Koi Fish Near Yellow Koi Fish";
+// imageE2.width = 360;
+// imageE2.height = 300;
+
+
+
+// const galleryList2 = document.querySelector(".gallery-list-2");
+// galleryList2.append(imageE2);
+
+
+// const imageE3 = document.createElement("img");
+// imageE3.src = "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260";
+// imageE3.alt = "Group of Horses Running";
+// imageE3.width = 360;
+// imageE3.height = 300;
+// const galleryList3 = document.querySelector(".gallery-list-3");
+// galleryList3.append(imageE3);
