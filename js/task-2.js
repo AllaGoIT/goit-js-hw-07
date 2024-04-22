67043203f0fc9e337d611a47b0892a92d60d6f99
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-04-22 11:57:28 +0300

    fix3

--- js/task-2.js
+++ js/task-2.js
@@ -25,108 +25,30 @@ const images = [
   }
 ];
 
-//let counter = 0;
-
-// const galleryArray = document.querySelector("ul.gallery");
-// images.forEach(function callback(element, index, array) {
-//   if (counter >= 3) {
-//     return;
-//   }
-//   const newEl = document.createElement("li");
-//   newEl.classList.add("gallery-list");
-//   galleryArray.append(newEl);
-  
-//   const imageEl = document.createElement("img");
-//   imageEl.src = element.url;
-//   imageEl.alt = element.alt;
-//   imageEl.width = 360;
-//   imageEl.height = 300;
-
-//  newEl.append(imageEl);
-    
-//   counter++;
-// })
 
 const galleryListEl = document.querySelector("ul.gallery");
 const fragment = document.createDocumentFragment();
 
+let counter = 0;
 images.forEach(function callback(element) {
+  if (counter >= 3) {
+  return;
+  }
 const li = document.createElement("li");
 li.classList.add("gallery-list");
 const imageEl = document.createElement("img");
 imageEl.src = element.url;
 imageEl.alt = element.alt;
 imageEl.width = 360;
-imageEl.height = 300;
+  imageEl.height = 300;
+  li.appendChild(imageEl);
 
-  fragment.appendChild(li, imageEl);
+  fragment.appendChild(li);
+
+  counter++;
 });
 
 galleryListEl.appendChild(fragment);
 
 
 
-// const galleryArray = document.querySelector("ul.gallery");
-// const newEl = document.createElement("li");
-// const imageEl = document.createElement("img");
-
-// images.forEach(function callback(element, _index) {
-//   if (counter >= 3) {
-// imageEl.src = element.url;
-// imageEl.alt = element.alt;
-// imageEl.width = 360;
-// imageEl.height = 300;
-//   return;
-//   }
-// counter++;
-// })
- 
-// const createElementArray = [];
-// const markup = createElementArray
-//   .map((imageEl) => `<li class="gallery-list">${imageEl}</li>`);
-
-// galleryArray.insertAdjacentHTML("beforeend", markup);
-
-
-
-
-
-
-//const galleryArray = document.querySelector("ul.gallery");
-// const firstItem = document.createElement("li");
-// firstItem.classList.add("gallery-list-1");
-// const secondItem = document.createElement("li");
-// secondItem.classList.add("gallery-list-2");
-// const thirdItem = document.createElement("li");
-// thirdItem.classList.add("gallery-list-3");
-// document.body.append(firstItem, secondItem, thirdItem);
-
-// const imageE1 = document.createElement("img");
-// imageE1.src = "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260";
-// imageE1.alt = "White and Black Long Fur Cat";
-// imageE1.width = 360;
-// imageE1.height = 300;
-
-
-// const galleryList1 = document.querySelector(".gallery-list-1");
-// galleryList1.append(imageE1);
-
-// const imageE2 = document.createElement("img");
-// imageE2.src = "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260";
-// imageE2.alt = "Orange and White Koi Fish Near Yellow Koi Fish";
-// imageE2.width = 360;
-// imageE2.height = 300;
-
-
-
-// const galleryList2 = document.querySelector(".gallery-list-2");
-// galleryList2.append(imageE2);
-
-
-// const imageE3 = document.createElement("img");
-// imageE3.src = "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260";
-// imageE3.alt = "Group of Horses Running";
-// imageE3.width = 360;
-// imageE3.height = 300;
-// const galleryList3 = document.querySelector(".gallery-list-3");
-// galleryList3.append(imageE3);
